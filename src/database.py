4c442be6cb533728e5fc99572a582b9441be4127
Md Danish Alam <mddanishniet@gmail.com>
2026-05-18 04:43:28 +0530

    add user profile dashboard with Google OAuth integration

---
+++ src/database.py
@@ -0,0 +1,40 @@
+from flask_sqlalchemy import SQLAlchemy
+
+from datetime import datetime
+
+from flask_login import UserMixin
+
+
+db = SQLAlchemy()
+
+
+class User(UserMixin, db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    google_id = db.Column(db.String(200), unique=True)
+
+    name = db.Column(db.String(200))
+
+    email = db.Column(db.String(200), unique=True)
+
+    profile_pic = db.Column(db.String(500))
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class ChatHistory(db.Model):
+
+    __tablename__ = "chat_history"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(db.Integer)
+
+    question = db.Column(db.Text)
+
+    answer = db.Column(db.Text)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
